e821f3af858008ce25c45b61afc909608d3cdcb3
Crowdbotics <team@crowdbotics.com>
2021-01-09 19:39:48 +0000

    Initial commit.

---
+++ ios/gaas_23745Tests/gaas_23745Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface gaas_23745 : XCTestCase
+
+@end
+
+@implementation gaas_23745
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
